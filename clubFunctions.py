e02daf7b48df7f43d56b0182cedb82adc8df8c99
Kevin McDonald <kmdon39@uncc.edu>
2021-11-17 23:27:54 -0500

    added initial flask stuff

---
+++ clubFunctions.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def getMembers():
+    members = 50
+    return render_template('club.html', members=members)
