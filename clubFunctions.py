49d856c3439b8354be2cc0690b15aa7e68af2d9d
Kevin McDonald <kmdon39@uncc.edu>
2021-11-19 01:46:42 -0500

    changes to html

--- clubFunctions.py
+++ clubFunctions.py
@@ -13,29 +13,59 @@ app.secret_key = "hello"
 def home():
     if "user" in session:
         user = session["user"]
-        return render_template("studentHome.html", user=user, login="logout")
+        return render_template("studentHome.html", user=user)
     else:
-        return render_template("studentHome.html", user=user, login="login")
-
-@app.route("/login.html", methods=["POST", "GET"])
-def login():
-    global usr
-    if request.method == "POST":
-        user = request.form["student"]
-        session["user"] = user
-        return redirect(url_for("home"))
-    else:
-        return render_template("login.html")
+        return render_template("studentHome.html")
 
 @app.route("/logout")
 def logout():
     session.pop("user", None)
     return redirect(url_for("login"))
 
+@app.route("/login.html", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["student"]
+        if user != "":
+            session["user"] = user
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html", message="Enter ID")
+    else:
+        return render_template("login.html")
+
 @app.route("/club.html")
-def getMembers():
-    members = 50
-    return render_template('club.html', members=members)
+def club():
+        if "user" in session:
+            members = 50
+            user = session["user"]
+            return render_template("club.html", user=user, members=members)
+        else:
+            members = 50
+            return render_template("club.html", members=members)
+
+@app.route("/findClubs.html")
+def findClubs():
+        if "user" in session:
+            user = session["user"]
+            return render_template("findClubs.html", user=user)
+        else:
+            return render_template("findClubs.html")
+
+@app.route("/submitClub.html")
+def submitClub():
+        if "user" in session:
+            user = session["user"]
+            return render_template("submitClub.html", user=user)
+        else:
+            return render_template("submitClub.html")
+@app.route("/myClubs.html")
+def myClubs():
+    if "user" in session:
+        user = session["user"]
+        return render_template("myClubs.html", user=user)
+    else:
+        return render_template("myClubs.html")
 
 if __name__ == "__main__":
     app.run()
